4e23c013e1b43174875526233acdddab0291482a
fabinthesky <f3dsign@gmail.com>
2015-03-03 18:40:38 +0100

    dynamic fixtures

--- src/Ens/JobeetBundle/DataFixtures/ORM/LoadJobData.php
+++ src/Ens/JobeetBundle/DataFixtures/ORM/LoadJobData.php
@@ -8,58 +8,28 @@ use Ens\JobeetBundle\Entity\Job;
 
 class LoadJobData extends AbstractFixture implements OrderedFixtureInterface
 {
+  
+  /**
+   * create job offers
+   *
+   */
   public function load(ObjectManager $em) {
-    $job_sensio_labs = new Job();
-    $job_sensio_labs->setCategory($em->merge($this->getReference('category-programming')));
-    $job_sensio_labs->setType('full-time');
-    $job_sensio_labs->setCompany('Sensio Labs');
-    $job_sensio_labs->setLogo('sensio-labs.gif');
-    $job_sensio_labs->setUrl('http://www.sensiolabs.com/');
-    $job_sensio_labs->setPosition('Web Developer');
-    $job_sensio_labs->setLocation('Paris, France');
-    $job_sensio_labs->setDescription('You\'ve already developed websites with symfony and you want to work with Open-Source technologies. You have a minimum of 3 years experience in web development with PHP or Java and you wish to participate to development of Web 2.0 sites using the best frameworks available.');
-    $job_sensio_labs->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
-    $job_sensio_labs->setIsPublic(true);
-    $job_sensio_labs->setIsActivated(true);
-    $job_sensio_labs->setToken('job_sensio_labs');
-    $job_sensio_labs->setEmail('job@example.com');
-    $job_sensio_labs->setCreatedAt(new \DateTime('2015-03-03'));
-    
-    $job_extreme_sensio = new Job();
-    $job_extreme_sensio->setCategory($em->merge($this->getReference('category-design')));
-    $job_extreme_sensio->setType('part-time');
-    $job_extreme_sensio->setCompany('Extreme Sensio');
-    $job_extreme_sensio->setLogo('extreme-sensio.gif');
-    $job_extreme_sensio->setUrl('http://www.extreme-sensio.com/');
-    $job_extreme_sensio->setPosition('Web Designer');
-    $job_extreme_sensio->setLocation('Paris, France');
-    $job_extreme_sensio->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.');
-    $job_extreme_sensio->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
-    $job_extreme_sensio->setIsPublic(true);
-    $job_extreme_sensio->setIsActivated(true);
-    $job_extreme_sensio->setToken('job_extreme_sensio');
-    $job_extreme_sensio->setEmail('job@example.com');
-    $job_extreme_sensio->setCreatedAt(new \DateTime('2015-03-03'));
-    
-    $job_expired_sensio = new Job();
-    $job_expired_sensio->setCategory($em->merge($this->getReference('category-design')));
-    $job_expired_sensio->setType('part-time');
-    $job_expired_sensio->setCompany('Extreme Sensio');
-    $job_expired_sensio->setLogo('extreme-sensio.gif');
-    $job_expired_sensio->setUrl('http://www.extreme-sensio.com/');
-    $job_expired_sensio->setPosition('Web Designer expired');
-    $job_expired_sensio->setLocation('Paris, France');
-    $job_expired_sensio->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.');
-    $job_expired_sensio->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
-    $job_expired_sensio->setIsPublic(true);
-    $job_expired_sensio->setIsActivated(true);
-    $job_expired_sensio->setToken('job_extreme_sensio');
-    $job_expired_sensio->setEmail('job@example.com');
-    $job_expired_sensio->setCreatedAt(new \DateTime('2012-10-10'));
-    
-    $em->persist($job_sensio_labs);
-    $em->persist($job_extreme_sensio);
-    
+    for($i = 100; $i <= 130; $i++) {
+        $job = new Job();
+        $job->setCategory($em->merge($this->getReference('category-programming')));
+        $job->setType('full-time');
+        $job->setCompany('Company '.$i);
+        $job->setPosition('Seigneur du Temps');
+        $job->setLocation('Gallifrey, constellation de Kasterborous');
+        $job->setDescription('omelette du fromage');
+        $job->setHowToApply('Send your resume to theDoctor@gmail.gal');
+        $job->setIsPublic(true);
+        $job->SetIsActivated(true);
+        $job->setToken('job_'.$i);
+        $job->setEmail('job@example.com');
+
+        $em->persist($job);
+    }
     $em->flush();
   }
   
