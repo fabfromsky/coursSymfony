3ba6067cdf3cd5cbf18e896298f8b55e9ccbc3e1
fabinthesky <f3dsign@gmail.com>
2015-03-05 15:56:31 +0100

    slugify method test

---
+++ src/Ens/JobeetBundle/Tests/Utils/JobeetTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Ens\JobeetBundle\Tests\Utils;
+use Ens\JobeetBundle\Utils\Jobeet;
+
+class JobeeTests extends \PHPUnit_Framework_TestCase
+{
+  /**
+   * Description
+   * @return type
+   */
+  public function testSlugify() {
+    $this->assertEquals('sensio', Jobeet::slugify('sensio'));
+    $this->assertEquals('sensio-labs', Jobeet::slugify('sensio labs'));
+    $this->assertEquals('sensio-labs', Jobeet::slugify('sensio labs'));
+    $this->assertEquals('paris-france', Jobeet::slugify('paris, france'));
+    $this->assertEquals('sensio', Jobeet::slugify('sensio'));
+    $this->assertEquals('sensio', Jobeet::slugify('sensio'));
+    $this->assertEquals('n-a', Jobeet::slugify(''));
+    $this->assertEquals('n-a', Jobeet::slugify(' - '));
+    if(function_exists('iconv')){
+      $this->assertEquals('developpeur-web', Jobeet::slugify('Développeur Web'));
+    }
+  }
+}
--- src/Ens/JobeetBundle/Utils/Jobeet.php
+++ src/Ens/JobeetBundle/Utils/Jobeet.php
@@ -12,11 +12,23 @@ class Jobeet
   static public function slugify($text) {
     
     //replace all special chars by -
-    $text = preg_replace('/\W+/', '-', $text);
+    $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
     
+    //to avoid transliteration bugs
+    if(function_exists('iconv')) {
+      $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    }
+
     //trim and lowercase
     $text = strtolower(trim($text, '-'));
     
+    //remove unwanted chars
+    $text = preg_replace('#[^-\w]+#', '', $text);
+    //if empty returns n-a
+    if(empty($text)) {
+      return 'n-a';
+    }
+
     return $text;
   }
 }
