1e236fee2c39001204db806ed0b5e2c0219ad52a
fabinthesky <f3dsign@gmail.com>
2015-03-30 19:16:16 +0200

    category job feed

--- src/Ens/JobeetBundle/Controller/CategoryController.php
+++ src/Ens/JobeetBundle/Controller/CategoryController.php
@@ -8,19 +8,18 @@ use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 /**
  * Controller category
- * 
+ *
  * @author fabinthesky
  *        
  */
 class CategoryController extends Controller {
 	
 	/**
-	*@ApiDoc(
-	*	resource=true,
-	*	description="show jobs by category"
-	*)
-	*
-	*/
+	 * @ApiDoc(
+	 * resource=true,
+	 * description="show jobs by category"
+	 * )
+	 */
 	public function showAction($slug, $page) {
 		$em = $this->getDoctrine ()->getManager ();
 		
@@ -30,6 +29,15 @@ class CategoryController extends Controller {
 			throw $this->createNotFoundException ( 'Unable to find Catergory entity.' );
 		}
 		
+		$latestJob = $em->getRepository ( 'EnsJobeetBundle:Job' )->getLatestPost ( $category->getId () );
+		
+		if ($latestJob) {
+			$lastUpdated = $latestJob->getCreatedAt ()->format ( DATE_ATOM );
+		} else {
+			$lastUpdated = new \DateTime ();
+			$lastUpdated = $lastUpdated->format ( DATE_ATOM );
+		}
+		
 		$total_jobs = $em->getRepository ( 'EnsJobeetBundle:Job' )->countActiveJobs ( $category->getId () );
 		$jobs_per_page = $this->container->getParameter ( 'max_jobs_on_category' );
 		$last_page = ceil ( $total_jobs / $jobs_per_page );
@@ -37,13 +45,20 @@ class CategoryController extends Controller {
 		$next_page = $page < $last_page ? $page + 1 : $last_page;
 		$category->setActiveJobs ( $em->getRepository ( 'EnsJobeetBundle:Job' )->getActiveJobs ( $category->getId (), $jobs_per_page, ($page - 1) * $jobs_per_page ) );
 		
-		return $this->render ( 'EnsJobeetBundle:Category:show.html.twig', array (
+		$format = $this->getRequest ()->getRequestFormat ();
+		
+		return $this->render ( 'EnsJobeetBundle:Category:show.' . $format . '.twig', array (
 				'category' => $category,
 				'last_page' => $last_page,
 				'previous_page' => $previous_page,
 				'current_page' => $page,
 				'next_page' => $next_page,
-				'total_jobs' => $total_jobs 
+				'total_jobs' => $total_jobs,
+				'feedId' => sha1 ( $this->get ( 'router' )->generate ( 'ens_jobeet_category', array (
+						'slug' => $category->getSlug (),
+						'format' => 'atom' 
+				), true ) ),
+				'lastUpdated' => $lastUpdated 
 		) );
 	}
 }
